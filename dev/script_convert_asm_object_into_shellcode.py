d25fd76494084283fcb8cab26b914dbb588529ca
Sithi5 <ma.sithis@gmail.com>
2021-07-31 14:55:38 +0200

    script parsing almost finish.

--- dev/script_convert_asm_object_into_shellcode.py
+++ dev/script_convert_asm_object_into_shellcode.py
@@ -2,24 +2,29 @@ import argparse
 import subprocess
 import platform
 import pathlib
+import re
 
 ACCEPTED_SYSTEM = ["Linux"]
 ACCEPTED_ARCHITECTURE = ["64bit"]
 ACCEPTED_FILE_EXTENSION = ["asm", "s"]
 
 
+def error(error_message: str = None):
+    print(error_message)
+    exit()
+
+
 def main(argv=None):
     if platform.system() not in "".join(ACCEPTED_SYSTEM) or platform.architecture()[
         0
     ] not in "".join(ACCEPTED_ARCHITECTURE):
-        print(
-            "This script need to be run with a "
+        error(
+            error_message="This script need to be run with a "
             + "/".join(ACCEPTED_SYSTEM)
             + " in a "
             + "/".join(ACCEPTED_ARCHITECTURE)
             + " architecture to work properly."
         )
-        exit()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("asm_file_name")
@@ -30,14 +35,32 @@ def main(argv=None):
     # Removing the dot for file_extension.
     file_extension = file_extension[1:]
     if file_extension not in ACCEPTED_FILE_EXTENSION:
-        print("Only " + "/".join(ACCEPTED_FILE_EXTENSION) + " files extensions are accepted.")
-        exit()
+        error(
+            error_message="Only "
+            + "/".join(ACCEPTED_FILE_EXTENSION)
+            + " files extensions are accepted."
+        )
     output = subprocess.getoutput("nasm -f elf64 " + file_name)
     if len(output) > 0:
-        print("An error occured with nasm: ", output)
-        exit()
+        error(error_message="An error occured with nasm: " + output)
     output = subprocess.getoutput("objdump -d " + file_base_name + ".o")
     print(output)
+    output = output.split("\n")
+    shell_code = []
+    pattern_match_code_line = r"[\da-f]+:"
+    pattern_hexa_code = r"^[a-f\d]+"
+    for line in output:
+        if re.search(pattern_match_code_line, line):
+            # Remove left part before ':' char
+            line = line.split(":")[1]
+            line = line.replace(" ", "")
+            line = line.replace("\t", "")
+            match = re.match(pattern_hexa_code, line)
+            if match is None:
+                error(error_message="Error during line matching.")
+            line = match.group(0)
+            shell_code.append(line)
+    print("\n".join(shell_code))
 
 
 if __name__ == "__main__":
